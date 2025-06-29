915d360147b36107f1ef25752fd2cebafc227995
chetankumar14 <mollichetankumar@gmail.com>
2025-06-29 20:39:58 +0530

    Add files via upload

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
-import store from './redux/store';
-import './index.css'; // You can add custom CSS here
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
